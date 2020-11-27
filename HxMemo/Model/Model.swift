b46517d99fd00babf939fb990483da3d7b9edbb8
Lee Ha Yeon <gkdus1613@icloud.com>
2020-11-27 19:38:27 +0900

    Commit_3

---
+++ HxMemo/Model/Model.swift
@@ -0,0 +1,23 @@
+//
+//  Model.swift
+//  HxMemo
+//
+//  Created by 이하연 on 2020/11/27.
+//
+
+import Foundation
+
+class Memo {
+    var content : String
+    var insertDate : Date
+    
+    init( content: String) {
+        self.content = content
+        insertDate = Date()
+    }
+    
+    static var dummyMemoList = [
+        Memo(content: "Hayeon"),
+        Memo(content: "Test")
+    ]
+}
