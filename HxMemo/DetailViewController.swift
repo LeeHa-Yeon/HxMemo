6e182769cdd46f36bdc37331c6a77fef1a821bed
Lee Ha Yeon <gkdus1613@icloud.com>
2020-11-30 16:59:21 +0900

    Commit_iPad

--- HxMemo/DetailViewController.swift
+++ HxMemo/DetailViewController.swift
@@ -30,10 +30,14 @@ class DetailViewController: UIViewController {
     }
     
     
-    @IBAction func share(_ sender: Any) {
+    @IBAction func share(_ sender: UIBarButtonItem) {
         guard let memo = memo?.content else{ return}
         let vc = UIActivityViewController(activityItems: [memo], applicationActivities: nil)
         present(vc, animated: true, completion: nil)
+        
+        if let pc = vc.popoverPresentationController {
+            pc.barButtonItem = sender
+        }
     }
     
     
